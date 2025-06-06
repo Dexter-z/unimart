fa3806ce78f2c6bc09c8586c4aa7d04baed892f6
Dexter2-z <chinazachinyere7@gmail.com>
2025-06-06 18:01:53 +0100

    forget tailwind for now

--- apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { AlignLeft, ChevronDown } from 'lucide-react';
+import { navItems } from 'apps/user-ui/src/configs/constants';
+import { AlignLeft, ChevronDown, HeartIcon, ShoppingCart, UserRound } from 'lucide-react';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const HeaderBottom = () => {
@@ -37,6 +39,47 @@ const HeaderBottom = () => {
 
             </div>
         )}
+
+        <div className='flex items-center'>
+            {navItems.map((i:NavItemsTypes, index:number) => (
+                <Link className='px-5 font-medium text-lg' href={i.href} key={index}>
+                    {i.title}
+                </Link>
+            ))}
+        </div>
+        <div>
+            {isSticky && (
+                <div className='flex items-center gap-8 pb-2'>
+                    <div className='flex items-center gap-2'>
+                        <Link href={"/login"} className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
+                            <UserRound />
+                        </Link>
+
+                        <Link href={"/login"}>
+                            <span className='block font-medium'>Hello,</span>
+                            <span className='font-semibold'>Sign in</span>
+                        </Link>
+                    </div>
+                    <div className='flex items-center gap-5'>
+                        <Link href={"/wishlist"} className='relative'>
+                            <HeartIcon />
+                            <div className='w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px]'>
+                                <span className='text-white font-medium text-sm'>0</span>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className='flex items-center gap-5'>
+                        <Link href={"/cart"} className='relative'>
+                            <ShoppingCart />
+                            <div className='w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px]'>
+                                <span className='text-white font-medium text-sm'>0</span>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </div>
       </div>
     </div>
   )
