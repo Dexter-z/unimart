a6db0269ef848a8b43a7e5435aa8759a001e19a7
Dexter2-z <chinazachinyere7@gmail.com>
2025-06-10 23:35:08 +0100

    Prisma for sellers completed 07:06:45

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -224,3 +224,27 @@ export const resetUserPassword = async (req: Request, res: Response, next: NextF
 
     }
 }
+
+//Register a new seller
+export const registerSeller = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        validateRegistrationData(req.body, "seller")
+        const { name, email } = req.body
+
+        const existingUser = await prisma.users.findUnique({ where: { email } })
+
+        if (existingUser) {
+            return next(new ValidationError("A User already exists with this email"))
+        }
+
+        await checkOtpRestrictions(email, next)
+        await trackOtpRequests(email, next)
+        await sendOtp(name, email, "user-activation-mail")
+
+        res.status(200).json({
+            message: "OTP sent to email. Please verify your account"
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
