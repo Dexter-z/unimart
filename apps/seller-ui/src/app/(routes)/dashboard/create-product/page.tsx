02e7248a8dca80594de44d2fbae535d0aa0b0faf
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-06 01:09:31 +0100

    Create product route created

---
+++ apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className = 'dark text-white'>
+      Create Product
+    </div>
+  )
+}
+
+export default page
--- apps/seller-ui/src/app/(routes)/dashboard/layout.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/layout.tsx
@@ -1,52 +1,33 @@
 "use client"
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { AppSidebar } from "@/components/app-sidebar"
-
 import { Separator } from "@/components/ui/separator"
 import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { usePathname } from 'next/navigation'
-import useSeller from '@/hooks/useSeller'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    // const pathName = usePathname()
-    // const {seller} = useSeller()
-
-    // console.log(seller)
-
-
     return (
-        <div className='dark'>
+        <div className="dark min-h-screen flex">
             <SidebarProvider>
                 <AppSidebar />
-                <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                        <div className="flex items-center gap-2 px-3">
-                            <SidebarTrigger />
-                            <Separator orientation="vertical" className="mr-2 h-4" />
-                            
-                        </div>
-                    </header>
-                    <div className="flex flex-1 flex-col gap-4 p-4">
-                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                            <div className="bg-muted/50 aspect-video rounded-xl" />
-                            <div className="bg-muted/50 aspect-video rounded-xl" />
-                            <div className="bg-muted/50 aspect-video rounded-xl" />
-                        </div>
-                        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-                    </div>
-                </SidebarInset>
-            </SidebarProvider>
-
-            <main className='flex-1'>
-                <div className='overflow-auto'>
-                    {children}
+                <div className="flex-1 flex flex-col">
+                    <SidebarInset>
+                        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+                            <div className="flex items-center gap-2 px-3">
+                                <SidebarTrigger />
+                                <Separator orientation="vertical" className="mr-2 h-4" />
+                            </div>
+                        </header>
+                    </SidebarInset>
+                    <main className="flex-1 overflow-auto">
+                        {children}
+                    </main>
                 </div>
-            </main>
+            </SidebarProvider>
         </div>
     )
 }
