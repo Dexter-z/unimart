fa515ea0399113f34e66d39f227617852468853b
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-06 16:37:57 +0100

    Image Upload Complete

--- apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
@@ -5,13 +5,13 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 
 const page = () => {
-    const { 
+    const {
         register,
         control,
         watch,
         setValue,
         handleSubmit,
-        formState: { errors } 
+        formState: { errors }
     } = useForm();
 
     const [openImageModal, setOpenImageModal] = useState(false)
@@ -24,10 +24,10 @@ const page = () => {
     }
 
     const handleImageChange = (file: File | null, index: number) => {
-        const updatedImages = [... images];
+        const updatedImages = [...images];
         updatedImages[index] = file;
 
-        if(index === images.length - 1 && images.length < 8){
+        if (index === images.length - 1 && images.length < 8) {
             updatedImages.push(null)
         }
 
@@ -35,17 +35,17 @@ const page = () => {
         setValue('images', updatedImages)
     }
 
-    const handleRemoveImage = (index:number) => {
+    const handleRemoveImage = (index: number) => {
         setImages((prevImages) => {
             let updatedImages = [...prevImages]
 
-            if(index === -1){
+            if (index === -1) {
                 updatedImages[0] = null
-            } else{
+            } else {
                 updatedImages.splice(index, 1)
             }
 
-            if(!updatedImages.includes(null) && updatedImages.length < 8){
+            if (!updatedImages.includes(null) && updatedImages.length < 8) {
                 updatedImages.push(null)
             }
 
@@ -60,26 +60,49 @@ const page = () => {
             className='w-full mx-auto p-8 shadow-md rounded-lg text-white'
             onSubmit={handleSubmit(onSubmit)}
         >
-            <h2 className = 'text-2xl py-2 font-semibold font-Poppins text-white'>
+            <h2 className='text-2xl py-2 font-semibold font-Poppins text-white'>
                 Create Product
             </h2>
 
-            <div className = 'flex items-center'>
-                <span className = 'text-[#80Deea] cursor-pointer'>Dashboard</span>
-                <ChevronRight size = {20} className = 'opacity-[.8]' />
+            <div className='flex items-center'>
+                <span className='text-[#80Deea] cursor-pointer'>Dashboard</span>
+                <ChevronRight size={20} className='opacity-[.8]' />
                 <span>Create Product</span>
             </div>
 
-            <div className = 'py-4 w-full flex gap-6'>
+            {/* Content Layout */}
+            <div className='py-4 w-full flex gap-6'>
                 {/* Left side; Image upload */}
-                <div className='w-[35%]'>
-                    <ImagePlaceHolder 
-                        setOpenImageModal = {setOpenImageModal}
-                        size='765 X 850'
-                        small={false}
-                        index={0}
-                        onImageChange={handleImageChange}
-                    />
+                <div className='md:w-[35%]'>
+                    {images?.length > 0 && (
+                        <ImagePlaceHolder
+                            setOpenImageModal={setOpenImageModal}
+                            size='765 X 850'
+                            small={false}
+                            index={0}
+                            onImageChange={handleImageChange}
+                        />
+                    )}
+                </div>
+
+                <div className='grid grid-cols-2 gap-3 mt-4'>
+                    {images.slice(1).map((_, index) => (
+                        <ImagePlaceHolder
+                            setOpenImageModal={setOpenImageModal}
+                            size='765 X 850'
+                            key={index}
+                            small={true}
+                            index={index + 1}
+                            onImageChange={handleImageChange}
+                        />
+                    ))}
+                </div>
+
+                {/* Middle and Right side for rest of form input */}
+                <div className='w-65%'>
+                    <div className='mt-6 flex justify-end gap-3'>
+
+                    </div>
                 </div>
 
             </div>
