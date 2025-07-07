bda4b07236b4662c90a827e5ad552beb4377a31f
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-07 18:23:31 +0100

    Before creating discount codes page 11:00

--- apps/seller-ui/src/app/(routes)/dashboard/discount-codes/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/discount-codes/page.tsx
@@ -1,11 +1,22 @@
+"use client"
+
+import axiosInstance from '@/utils/axiosInstance'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 const Page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { data: discountCodes = [], isLoading } = useQuery({
+        queryKey: ["shop-discounts"],
+        queryFn: async () => {
+            const res = await axiosInstance.get("/product/api/get-discount-codes")
+            return res?.data?.discount_codes || []
+        }
+    })
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Page
