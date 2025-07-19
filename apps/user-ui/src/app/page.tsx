eb8f342f907d166db725c93eb116db185d0cc196
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-19 17:28:08 +0100

    Suggested products component done

--- apps/user-ui/src/app/page.tsx
+++ apps/user-ui/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '@/components/hero-section';
+import SuggestedProductsSection from '@/components/suggested-products-section';
 
 const Page = () => {
   return (
     <>
       <HeroSection />
+      <SuggestedProductsSection />
       <div className='h-[200vh]'>
-        
+        {/* ...rest of the homepage... */}
       </div>
     </>
   )
