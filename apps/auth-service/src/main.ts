1578c69a81caf3cd696828d54553a9eb172bbc15
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-19 23:46:44 +0100

    ips changed in main pages

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "http://192.168.130.215:3000"
+        "http://192.168.250.215:3000"
     ],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true
--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "http://192.168.130.215:3000"
+        "http://192.168.250.215:3000"
     ],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true
