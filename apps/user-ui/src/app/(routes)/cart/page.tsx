c375e2c69e25cbe8ec1679daef3d74c7c48f210a
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-28 19:33:59 +0100

    Cart page height increased

--- apps/user-ui/src/app/(routes)/cart/page.tsx
+++ apps/user-ui/src/app/(routes)/cart/page.tsx
@@ -4,7 +4,7 @@ import CartItems from "@/components/cart-items";
 
 const CartPage = () => {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 bg-[#18181b] text-white">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 bg-[#18181b] h-[100vh] text-white">
       <h1 className="text-3xl font-extrabold text-white mb-8">
         Shopping Cart
       </h1>
