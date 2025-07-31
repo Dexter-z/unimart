f0f13b9c8e264b289ea1fd682a25a9d06416ac03
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-31 23:40:28 +0100

    2:30 Aliases done

--- apps/kafka-service/src/main.ts
+++ apps/kafka-service/src/main.ts
@@ -1,17 +1,2 @@
+import {} from "@packages/utils/kafka";
 
-import express from 'express';
-import * as path from 'path';
-
-const app = express();
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to kafka-service!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
