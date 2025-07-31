49689999bf931860ee387572b3657cd664c90abb
Dexter2-z <chinazachinyere7@gmail.com>
2025-07-31 17:11:57 +0100

    White spaces removed from cart page

--- apps/user-ui/src/app/(routes)/cart/page.tsx
+++ apps/user-ui/src/app/(routes)/cart/page.tsx
@@ -4,11 +4,13 @@ import CartItems from "@/components/cart-items";
 
 const CartPage = () => {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 bg-[#18181b] h-[100vh] text-white">
-      <h1 className="text-3xl font-extrabold text-white mb-8">
-        Shopping Cart
-      </h1>
-      <CartItems />
+    <div className="w-full px-4 sm:px-6 lg:px-8 py-12 bg-[#18181b] min-h-screen text-white">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-3xl font-extrabold text-white mb-8">
+          Shopping Cart
+        </h1>
+        <CartItems />
+      </div>
     </div>
   );
 };
