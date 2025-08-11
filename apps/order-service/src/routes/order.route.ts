58363f2f57803f7113a94c6166755b2dbe48e4c0
Dexter2-z <chinazachinyere7@gmail.com>
2025-08-11 19:47:21 +0100

    Seller dashboard issue fixed

--- apps/order-service/src/routes/order.route.ts
+++ apps/order-service/src/routes/order.route.ts
@@ -1,17 +1,17 @@
-import isAuthenticated from "@packages/middleware/isAuthenticated";
+import isAuthenticated, { isSellerAuthenticated, isUserAuthenticated } from "@packages/middleware/isAuthenticated";
 import express, { Router } from "express";
 import { createPaymentIntent, createPaymentSession, getOrderDetails, getSellerOrders, getSellerPayments, updateOrderStatus, verifyingPaymentSession } from "../controllers/order.controller";
 import { isSeller } from "@packages/middleware/authorizeRoles";
 
 const router:Router = express.Router();
 
-router.post("/create-payment-intent", isAuthenticated, createPaymentIntent)
-router.post("/create-payment-session", isAuthenticated, createPaymentSession)
-router.get("/verifying-payment-session", isAuthenticated, verifyingPaymentSession)
-router.get("/get-seller-orders", isAuthenticated,isSeller, getSellerOrders)
-router.get("/get-seller-payments", isAuthenticated, isSeller, getSellerPayments)
+router.post("/create-payment-intent", isUserAuthenticated, createPaymentIntent)
+router.post("/create-payment-session", isUserAuthenticated, createPaymentSession)
+router.get("/verifying-payment-session", isUserAuthenticated, verifyingPaymentSession)
+router.get("/get-seller-orders", isSellerAuthenticated, getSellerOrders)
+router.get("/get-seller-payments", isSellerAuthenticated, getSellerPayments)
 router.get("/get-order-details/:id", isAuthenticated, getOrderDetails) 
-router.put("/update-status/:orderId", isAuthenticated, isSeller, updateOrderStatus)
+router.put("/update-status/:orderId", isSellerAuthenticated, updateOrderStatus)
 
 
 export default router;
--- apps/product-service/src/routes/product.routes.ts
+++ apps/product-service/src/routes/product.routes.ts
@@ -1,22 +1,22 @@
 import express, { Router } from "express";
 import { createDiscountCodes, createProduct, deleteDiscountCodes, deleteProduct, deleteProductImage, getAllProducts, getCategories, getDiscountCodeByName, getDiscountCodes, getShopProducts, restoreProduct, uploadProductImage, searchProducts, searchShops, getProductDetails, getFilteredEvents, getFilteredProducts, getFilteredShops, topShops, getRecommendedProducts } from "../controllers/product.controller";
-import isAuthenticated from "@packages/middleware/isAuthenticated";
+import isAuthenticated, { isSellerAuthenticated, isUserAuthenticated } from "@packages/middleware/isAuthenticated";
 
 const router: Router = express.Router();
 
 router.get("/search-products", searchProducts)
 router.get("/search-shops", searchShops)
 router.get("/get-categories", getCategories)
-router.post("/create-discount-code",isAuthenticated, createDiscountCodes);
-router.get("/get-discount-codes",isAuthenticated, getDiscountCodes);
+router.post("/create-discount-code", isSellerAuthenticated, createDiscountCodes);
+router.get("/get-discount-codes", isSellerAuthenticated, getDiscountCodes);
 router.get("/get-discount-code/:code", getDiscountCodeByName);
-router.delete("/delete-discount-code/:id",isAuthenticated, deleteDiscountCodes);
-router.post("/upload-product-image", isAuthenticated, uploadProductImage)
-router.delete("/delete-product-image", isAuthenticated, deleteProductImage)
-router.post("/create-product", isAuthenticated, createProduct)
-router.get("/get-shop-products", isAuthenticated, getShopProducts)
-router.delete("/delete-product/:productId", isAuthenticated, deleteProduct)
-router.put("/restore-product/:productId", isAuthenticated, restoreProduct)
+router.delete("/delete-discount-code/:id", isSellerAuthenticated, deleteDiscountCodes);
+router.post("/upload-product-image", isSellerAuthenticated, uploadProductImage)
+router.delete("/delete-product-image", isSellerAuthenticated, deleteProductImage)
+router.post("/create-product", isSellerAuthenticated, createProduct)
+router.get("/get-shop-products", isSellerAuthenticated, getShopProducts)
+router.delete("/delete-product/:productId", isSellerAuthenticated, deleteProduct)
+router.put("/restore-product/:productId", isSellerAuthenticated, restoreProduct)
 router.get("/get-all-products",  getAllProducts)
 router.get("/get-product/:slug", getProductDetails)
 router.get("/get-filtered-products", getFilteredProducts) 
