77ba55bdc7619112ed2f2a39f998d5ee9ed859d1
Dexter2-z <chinazachinyere7@gmail.com>
2025-08-11 18:24:42 +0100

    That otp issue at the beginning fixed

--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -22,13 +22,13 @@ export const validateRegistrationData = (data: any, userType: "user" | "seller")
 
 export const checkOtpRestrictions = async (email:string, next:NextFunction) => {
     if(await redis.get(`otp_lock:${email}`)){
-        return next(new ValidationError("Account temporarily locked due to multiple failed attempts!. Try again after 30 minutes"))
+        throw new ValidationError("Account temporarily locked due to multiple failed attempts!. Try again after 30 minutes")
     }
     if(await redis.get(`otp_spam_lock:${email}`)){
-        return next(new ValidationError("Too many otp requests!. Please wait 1 hour before requesting for another"))
+        throw new ValidationError("Too many otp requests!. Please wait 1 hour before requesting for another")
     }
     if(await redis.get(`otp_cooldown:${email}`)){
-        return next(new ValidationError("Please wait 1 minute before requesting for a new otp"))
+        throw new ValidationError("Please wait 1 minute before requesting for a new otp")
     }
 }
 
@@ -38,7 +38,7 @@ export const trackOtpRequests = async(email:string, next:NextFunction) => {
 
     if(otpRequests >= 3){
         await redis.set(`otp_spam_lock:${email}`, "locked", "EX", 3600)
-        return next(new ValidationError("Too many otp requests. Please try again after 1 hour")) 
+        throw new ValidationError("Too many otp requests. Please try again after 1 hour")
     }
 
     await redis.set(otpRequestKey,otpRequests + 1, "EX", 3600)
