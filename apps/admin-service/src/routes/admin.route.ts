24659425acbc702949a8e41bdbd0cb8711656c41
Dexter2-z <chinazachinyere7@gmail.com>
2025-08-26 17:17:56 +0100

    2:08 Admin routes and controller done

--- apps/admin-service/src/routes/admin.route.ts
+++ apps/admin-service/src/routes/admin.route.ts
@@ -1,9 +1,16 @@
 import express, { Router } from "express";
-import { getAllProducts } from "../controllers/admin.controller";
+import { addNewAdmin, getAllAdmins, getAllCustomizations, getAllEvents, getAllProducts, getAllSellers, getAllUsers } from "../controllers/admin.controller";
 import { isAdminAuthenticated } from "@packages/middleware/isAuthenticated";
 
 const router:Router = express.Router();
 
 router.get("/get-all-products",isAdminAuthenticated, getAllProducts)
+router.get("/get-all-events",isAdminAuthenticated, getAllEvents)
+router.get("/get-all-admins",isAdminAuthenticated, getAllAdmins)
+router.put("/add-new-admin",isAdminAuthenticated, addNewAdmin)
+router.get("/get-all-sellers",isAdminAuthenticated, getAllSellers)
+router.get("/get-all-users",isAdminAuthenticated, getAllUsers)
+router.get("/get-all-customizations",isAdminAuthenticated, getAllCustomizations)
+
 
 export default router;
