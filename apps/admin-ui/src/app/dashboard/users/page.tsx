925280db1584253ef4cf7c327159f5f38d8e9be4
Dexter2-z <chinazachinyere7@gmail.com>
2025-08-26 18:03:49 +0100

    Before users page creation

--- apps/admin-ui/src/app/dashboard/users/page.tsx
+++ apps/admin-ui/src/app/dashboard/users/page.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-export default function UsersPage() {
+import React from 'react'
+
+const UsersPage = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <h1 className="text-3xl font-bold text-[#ff8800]">Users</h1>
+    <div>
+      
     </div>
-  );
+  )
 }
+
+export default UsersPage
+
