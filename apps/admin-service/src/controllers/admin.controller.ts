829240ea8232a34e590610dc913bdfb0ee5991d9
Dexter2-z <chinazachinyere7@gmail.com>
2025-08-26 17:13:48 +0100

    2:07 Admin controller done

--- apps/admin-service/src/controllers/admin.controller.ts
+++ apps/admin-service/src/controllers/admin.controller.ts
@@ -166,3 +166,89 @@ export const getAllCustomizations = async (req: Request, res: Response, next: Ne
         return next(error)
     }
 }
+
+//Get All sellers
+export const getAllSellers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 20;
+        const skip = (page - 1) * limit;
+
+        const [sellers, totalSellers] = await Promise.all([
+            prisma.sellers.findMany({
+                skip,
+                take: limit,
+                orderBy: { createdAt: "desc" },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    phone_number: true,
+                    createdAt: true,
+                    shop: {
+                        select: {
+                            name: true,
+                            avatar: true,
+                            address: true,
+                        }
+                    },
+                }
+            }),
+            prisma.sellers.count()
+        ]);
+
+        const totalPages = Math.ceil(totalSellers / limit);
+
+        return res.status(200).json({
+            success: true,
+            data: sellers,
+            meta: {
+                totalSellers,
+                currentPage: page,
+                totalPages
+            }
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+//Get All users
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 20;
+        const skip = (page - 1) * limit;
+
+        const [users, totalUsers] = await Promise.all([
+            prisma.users.findMany({
+                skip,
+                take: limit,
+                orderBy: { createdAt: "desc" },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    phone_number: true,
+                    role: true,
+                    createdAt: true,
+                }
+            }),
+            prisma.users.count()
+        ]);
+
+        const totalPages = Math.ceil(totalUsers / limit);
+
+        return res.status(200).json({
+            success: true,
+            data: users,
+            meta: {
+                totalUsers,
+                currentPage: page,
+                totalPages
+            }
+        });
+    } catch (error) {
+        next(error)
+    }
+}
