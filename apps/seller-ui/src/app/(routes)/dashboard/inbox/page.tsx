3eaac1abfb1711bd87a3f1f90abccda5ff2afed7
Dexter2-z <chinazachinyere7@gmail.com>
2025-09-12 13:30:34 +0100

    04:25 before vibing the UI

--- apps/seller-ui/src/app/(routes)/dashboard/inbox/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/inbox/page.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client"
+
+import { useWebSocket } from '@/context/web-socket-context'
+import useSeller from '@/hooks/useSeller'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react'
 
 const InboxPage = () => {
+    const searchParams = useSearchParams()
+    const router = useRouter();
+    const messageContainerRef = useRef<HTMLDivElement | null>(null);
+    const {seller, isLoading: userLoading} = useSeller()
+    const conversationID = searchParams.get("conversationId")
+    const {ws} = useWebSocket()
+
+    const [chats, setChats] = useState<any[]>([])
+    const [selectedChat, setSelectedChat] = useState<any | null>(null);
+    const [message, setMessage] = useState("")
+
+    
   return (
     <div>
       hiii
