758e996315255df0e06d7e59ff1ef6c9504465bb
Dexter2-z <chinazachinyere7@gmail.com>
2025-09-17 08:56:39 +0100

    5:07 Metadata for shop done

--- apps/user-ui/src/app/(routes)/shop/[id]/page.tsx
+++ apps/user-ui/src/app/(routes)/shop/[id]/page.tsx
@@ -1,4 +1,43 @@
+import axiosInstance from '@/utils/axiosInstance'
+import { Metadata } from 'next';
 import React from 'react'
+
+async function fetchSellerDetails(id: string){
+    const res = await axiosInstance.get(`/seller/api/get-seller/${id}`);
+    return res.data;
+}
+
+//Generate meta data
+export async function generateMetadata({
+    params,
+} : {
+    params: { id: string };
+}) : Promise<Metadata> {
+    const data = await fetchSellerDetails(params.id);
+    return {
+        title: `${data?.shop?.name} | UniMart`,
+        description: data?.shop?.bio || `Welcome to ${data?.shop?.name} on UniMart. Explore our wide range of products and enjoy seamless shopping experience.`,
+        openGraph: {
+            title: `${data?.shop?.name} | UniMart`,
+            description: data?.shop?.bio || `Welcome to ${data?.shop?.name} on UniMart. Explore our wide range of products and enjoy seamless shopping experience.`,
+            type: "website",
+            images: [
+                {
+                    url: data?.shop?.avatar,
+                    width: 800,
+                    height: 600,
+                    alt: data?.shop?.name,
+                }
+            ]
+        },
+        twitter: {
+            card: "summary_large_image",
+            title: `${data?.shop?.name} | UniMart`,
+            description: data?.shop?.bio || `Welcome to ${data?.shop?.name} on UniMart. Explore our wide range of products and enjoy seamless shopping experience.`,
+            images: [data?.shop?.avatar]
+        }
+    }
+}
 
 const ShopPage = () => {
   return (
