07371ae6137595e793fe0b918802fc138be69ac8
Dexter2-z <chinazachinyere7@gmail.com>
2025-09-19 14:52:48 +0100

    2 seller controllers added

--- apps/seller-service/src/routes/seller.route.ts
+++ apps/seller-service/src/routes/seller.route.ts
@@ -1,7 +1,10 @@
-import express, { Router } from "express";
+import { Router } from 'express';
+import { isSellerAuthenticated } from '@packages/middleware/isAuthenticated';
+import { getSellerNotifications, markAsRead } from '../controllers/seller.controllers';
 
-const router:Router = express.Router();
+const router = Router();
 
-
+router.get('/seller-notifications', isSellerAuthenticated, getSellerNotifications);
+router.put('/mark-as-read/:notificationId', isSellerAuthenticated, markAsRead);
 
 export default router;
