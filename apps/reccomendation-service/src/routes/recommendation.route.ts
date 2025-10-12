73425709495103ede49edc6fa0946046685888d7
Dexter2-z <chinazachinyere7@gmail.com>
2025-10-12 16:26:34 +0100

    Auth erors fixed

--- apps/reccomendation-service/src/routes/recommendation.route.ts
+++ apps/reccomendation-service/src/routes/recommendation.route.ts
@@ -1,10 +1,10 @@
 import express, { Router } from "express";
 
-import isAuthenticated, { isAdminAuthenticated } from "@packages/middleware/isAuthenticated";
+import isAuthenticated, { isAdminAuthenticated, isUserAuthenticated } from "@packages/middleware/isAuthenticated";
 import { getRecommendedProducts } from "../controllers/recommendation.controller";
 
 const router:Router = express.Router();
 
-router.get("/get-recommendation-products", isAuthenticated, getRecommendedProducts)
+router.get("/get-recommendation-products", isUserAuthenticated, getRecommendedProducts)
 
 export default router;
