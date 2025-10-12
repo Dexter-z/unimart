bfaefee3e60b85dece135db00b968b7dfdd4b35a
Dexter2-z <chinazachinyere7@gmail.com>
2025-10-12 13:20:15 +0100

    Before editing recommendation service

--- apps/reccomendation-service/src/controllers/recommendation.controller.ts
+++ apps/reccomendation-service/src/controllers/recommendation.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction,  Response } from "express";
 import { recommendProducts } from "../services/recommendationService";
 import prisma from "@packages/libs/prisma";
 
