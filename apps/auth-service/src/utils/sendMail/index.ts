bec3308f672142ea5f0c38b194c176ca23f37b7c
Dexter2-z <chinazachinyere7@gmail.com>
2025-12-25 20:58:53 +0100

    fix: update email transporter configuration to use Resend service and environment variables

--- apps/auth-service/src/utils/sendMail/index.ts
+++ apps/auth-service/src/utils/sendMail/index.ts
@@ -2,13 +2,23 @@ import nodemailer from "nodemailer"
 import ejs from "ejs"
 import path from "path"
 
+// const transporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT) || 587,
+//     service: process.env.SMTP_SERVICE,
+//     auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS
+//     }
+// })
+
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
+    host: 'smtp.resend.com',
     port: Number(process.env.SMTP_PORT) || 587,
-    service: process.env.SMTP_SERVICE,
+    secure: true,
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: 'resend',
+        pass: process.env.RESEND_API_KEY,
     }
 })
 
